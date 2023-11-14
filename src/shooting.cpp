d1d19476bdc99782a1356cb604228ba67a77bc8a
ycleeag267 <ycleeag@gmail.com>
2023-11-13 20:23:57 -0600

    timing changes

--- src/shooting.cpp
+++ src/shooting.cpp
@@ -17,11 +17,13 @@ void shooting::setup() {
 void shooting::shoot(){
     //Shooting Code, may need to adjust delays
     digitalWrite(Solenoid, HIGH); 
-    myStepper.step(-125);     //Switch Solenoid ON
+    delay(100);
+    digitalWrite(Solenoid, LOW); 
+    myStepper.step(-130);     //Switch Solenoid ON
     //delay(100);                          
-    digitalWrite(Solenoid, LOW);       //Switch Solenoid OFF
+          //Switch Solenoid OFF
     //Reload code
     //need to verify rotation duration/trial&error?
     //delay(300);
-    myStepper.step(125);
+    myStepper.step(130);
 }
